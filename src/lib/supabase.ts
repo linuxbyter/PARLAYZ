bb072cccf2645a7ee90d19d2f621e10c77f9a227
linuxbyter <makau1.peter@gmail.com>
2026-02-24 23:47:55 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL'
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY'
+const supabaseUrl = 'https://blwczecyysitydcdwghj.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsd2N6ZWN5eXNpdHlkY2R3Z2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5MjcxMTksImV4cCI6MjA4NzUwMzExOX0.bQfbmkdWEeLz7SSdboKi4SWmSppifuu5Auc9BpnRyUw'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
