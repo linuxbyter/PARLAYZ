9d6ef65210bde2341bbff89b816a4f0d2bf176b8
linuxbyter <makau1.peter@gmail.com>
2026-02-27 00:08:47 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
+import Admin from './Admin.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* The main app everyone sees */}
+        <Route path="/" element={<App />} />
+        
+        {/* The hidden admin dashboard */}
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
